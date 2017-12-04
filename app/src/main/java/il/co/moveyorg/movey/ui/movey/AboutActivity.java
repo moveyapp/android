d3feba707273b1daa31f4029ba6723df5a2b0fe5
eladkeyshawn <elad.keyshawn@gmail.com>
2017-12-04 13:45:02 +0200

    add cool temp launcher icon and edit about activity

--- app/src/main/java/il/co/moveyorg/movey/ui/movey/AboutActivity.java
+++ app/src/main/java/il/co/moveyorg/movey/ui/movey/AboutActivity.java
@@ -2,6 +2,7 @@ package il.co.moveyorg.movey.ui.movey;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.Gravity;
 import android.view.View;
@@ -9,28 +10,30 @@ import android.widget.Toast;
 
 import java.util.Calendar;
 
+import il.co.moveyorg.movey.R;
 import il.co.moveyorg.movey.ui.base.BaseActivity;
 import mehdi.sakout.aboutpage.AboutPage;
 import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends BaseActivity {
-
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         simulateDayNight(/* DAY */ 0);
-        Element adsElement = new Element();
-        adsElement.setTitle("Advertise with us");
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
-                .addItem(new Element().setTitle("Version 0.1"))
-                .addItem(adsElement)
-                .addGroup("Connect with us")
+                .setDescription(getString(R.string.app_desc))
+                .setImage(R.drawable.logo__blue)
+                .addItem(new Element().setTitle("Version 0.0.1"))
                 .addEmail("shlomi.al90@gmail.com")
                 .addWebsite("http://softpillow.me/")
                 .addPlayStore("com.ideashower.readitlater.pro")
-                .addGitHub("https://github.com/moveyapp")
                 .addItem(getCopyRightsElement())
                 .create();
 
@@ -40,17 +43,12 @@ public class AboutActivity extends BaseActivity {
 
     Element getCopyRightsElement() {
         Element copyRightsElement = new Element();
-        final String copyrights = String.format("bla bla", Calendar.getInstance().get(Calendar.YEAR));
+        final String copyrights = String.format("Open source licences", Calendar.getInstance().get(Calendar.YEAR));
         copyRightsElement.setTitle(copyrights);
         copyRightsElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_item_icon_color);
         copyRightsElement.setIconNightTint(android.R.color.white);
         copyRightsElement.setGravity(Gravity.CENTER);
-        copyRightsElement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(AboutActivity.this, copyrights, Toast.LENGTH_SHORT).show();
-            }
-        });
+        copyRightsElement.setOnClickListener(v -> Toast.makeText(AboutActivity.this, copyrights, Toast.LENGTH_SHORT).show());
         return copyRightsElement;
     }
 
